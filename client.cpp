3caaa4585e318df0ce2b6475c9ef02ec63a4d231
m063085962 <1157230430@qq.com>
2024-06-24 19:41:12 +0800

    add error check and build an echo server

--- client.cpp
+++ client.cpp
@@ -1,23 +1,56 @@
 #include<iostream>
 #include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
+#include<unistd.h>
+
+void errif(bool condition, const char *errmsg)
+{
+	if(condition){
+		perror(errmsg);
+		exit(EXIT_FAILURE);
+	}
+}
 
 int main()
 {
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	
+	errif(sock==-1, "socket create error");
+
 	struct sockaddr_in serv_addr;
 	bzero(&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serv_addr.sin_port = htons(8888);
-	connect(sock, (sockaddr*)&serv_addr, sizeof(serv_addr));
+	errif(connect(sock, (sockaddr*)&serv_addr, sizeof(serv_addr))==-1, "socket connect error");
+
 	std::cout << "connect successful" << std::endl;
+	std::cout << "server ip:" << inet_ntoa(serv_addr.sin_addr) << " port:" << ntohs(serv_addr.sin_port) << std::endl;
+
+	while(true)
+	{
+		char buf[1024];
+		bzero(&buf, sizeof(buf));
+		std::cin >> buf;
+		ssize_t write_bytes = write(sock, buf, sizeof(buf));
+		if(write_bytes == -1){
+			std::cout << "socket disconnected, can't write any more!" << std::endl;
+			break;
+		}
+		bzero(&buf, sizeof(buf));
+		ssize_t read_bytes = read(sock, buf, sizeof(buf));
+		if(read_bytes > 0){
+			std::cout << "from server: " << buf << std::endl;
+		}else if(read_bytes == 0){
+			std::cout << "server socket disconnected" << std::endl;
+			break;
+		}else if(read_bytes == -1){
+			close(sock);
+			errif(true, "socket read error");
+		}
+	}
 	
-	std::cout << "ip: " << inet_ntoa(serv_addr.sin_addr) << std::endl;
-	std::cout << "port: " << ntohs(serv_addr.sin_port) << std::endl;
-	//char buffer[40];
-	//read(sock, buffer, sizeof(buffer)-1);
 	return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,29 +1,59 @@
 #include<iostream>
 #include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
+#include<unistd.h>
+
+void errif(bool condition, const char *errmsg)
+{
+	if(condition){
+		perror(errmsg);
+		exit(EXIT_FAILURE);
+	}
+}
 
 int main()
 {
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	int serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+	errif(serv_sock == -1, "socket create error");
 
 	struct sockaddr_in serv_addr;
 	bzero(&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serv_addr.sin_port = htons(8888);
-	bind(sock, (sockaddr*)&serv_addr, sizeof(serv_addr));
+	errif(bind(serv_sock, (sockaddr*)&serv_addr, sizeof(serv_addr))==-1, "socket bind error");
 
-	listen(sock, SOMAXCONN);
+	errif(listen(serv_sock, SOMAXCONN)==-1, "socket listen error");
 
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_len = sizeof(clnt_addr);
 	bzero(&clnt_addr, sizeof(clnt_addr));
-	std::cout << "waiting connection..." << std::endl;
-	int clnt_sock = accept(sock, (sockaddr*)&clnt_addr, &clnt_addr_len);
+	int clnt_sock = accept(serv_sock, (sockaddr*)&clnt_addr, &clnt_addr_len);
+	errif(clnt_sock==-1, "socket accept error");
+	
+	std::cout << "connect successful" << std::endl;
+	std::cout << "client ip:" << inet_ntoa(clnt_addr.sin_addr) << " port:" << ntohs(clnt_addr.sin_port) << std::endl;
 
-	std::cout << "ip: " << inet_ntoa(clnt_addr.sin_addr) << std::endl;
-	std::cout << "port: " << ntohs(clnt_addr.sin_port) << std::endl;
+	while(true)
+	{
+		char buf[1024];
+		bzero(&buf, sizeof(buf));
+		ssize_t read_bytes = read(clnt_sock, buf, sizeof(buf));
+		if(read_bytes > 0){
+			std::cout << "from client: " << buf << std::endl;
+			write(clnt_sock, buf ,sizeof(buf));
+		}else if(read_bytes == 0){
+			std::cout << "client disconnected" << std::endl;
+			close(clnt_sock);
+			break;
+		}else if(read_bytes == -1){
+			close(clnt_sock);
+			errif(true, "socket read error");
+		}
+	}
 
 	return 0;
 }
