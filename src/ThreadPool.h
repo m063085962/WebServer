c10ad99cb9552dc3900cb13b981bb38e8b177679
m063085962 <1157230430@qq.com>
2024-07-28 19:08:13 +0800

    rewrite add() of ThreadPool.cpp

--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -5,6 +5,7 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <future>
 
 class ThreadPool
 {
@@ -17,6 +18,25 @@ private:
 public:
 	ThreadPool(int siez=10);
 	~ThreadPool();
-	void add(std::function<void()>);
+
+	template<class F, class... Args>
+	auto add(F&& f, Args&&... args)->std::future<typename std::result_of<F(Args...)>::type>;
 };
+
+template<class F, class... Args>
+auto ThreadPool::add(F&& f, Args&&... args)->std::future<typename std::result_of<F(Args...)>::type>
+{
+	using return_type = typename std::result_of<F(Args...)>::type;
+	auto task = std::make_shared< std::packaged_task<return_type()> >(
+			std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+	std::future<return_type> res = task->get_future();
+	{
+		std::unique_lock<std::mutex> lock(tasks_mtx);
+		if(stop)
+			throw std::runtime_error("enqueue on stopped ThreadPool");
+		tasks.emplace([task](){(*task)();});
+	}
+	cv.notify_one();
+	return res;
+}
 
